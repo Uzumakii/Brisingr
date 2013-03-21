f54cd2fc9dbd9ada091f31b50949f05cf3252590
Krzysztof Pazdur <pazdurk@gmail.com>
2013-03-21 20:18:48 +0100

    Wczytywanie wielu plików php.

--- install.php
+++ install.php
@@ -19,12 +19,21 @@
  *
  ***************************************************************************/
 
-/**
+/*
 	This program was written by Krzysztof Pazdur and Tomasz Tomala, members of Brisingr Group.  
 	It is main install scirpt. Ours program creating list directiores, indexing files,
 	saving passwords and more.
 	After all, this program creating file config.php with settings and defining IS_INSTALLED as True.
 */
+	# Yes ... 
+	define('Brisingr',true);
 	
-
+	$files_to_include = glob('./install/DB_*.php');
+		
+		foreach( $files_to_include as $key => $val ) 
+		{
+			include($val);	
+		}
+		
+		
 ?>
