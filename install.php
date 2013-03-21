fed81d05db9cbb50767626f2262b896d4ef313de
Krzysztof Pazdur <pazdurk@gmail.com>
2013-03-21 16:49:50 +0100

    Tworzenie struktury baz itp.

---
+++ install.php
@@ -0,0 +1,30 @@
+﻿<?php
+/***************************************************************************
+ *                              install.php
+ *                            -------------------
+ *   begin                : Thursday, March 21, 2013
+ *   copyright            : (C) 2013 The Brisingr Group
+ *   email                : <your@email.com>
+ *
+ *
+ *
+ ***************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+
+/**
+	This program was written by Krzysztof Pazdur and Tomasz Tomala, members of Brisingr Group.  
+	It is main install scirpt. Ours program creating list directiores, indexing files,
+	saving passwords and more.
+	After all, this program creating file config.php with settings and defining IS_INSTALLED as True.
+*/
+	
+
+?>
